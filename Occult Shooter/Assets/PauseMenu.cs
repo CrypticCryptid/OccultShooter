6d8061020c82f36200e62b45efed49df9f7dec7d
CrypticCryptid <aege1.1.2002@gmail.com>
2024-05-26 14:16:16 -0700

    Lock and Unlock Cursor Added
    The cursor from the CameraController script can now be locked and unlocked. This allows for proper menu usage

--- Occult Shooter/Assets/PauseMenu.cs
+++ Occult Shooter/Assets/PauseMenu.cs
@@ -8,6 +8,11 @@ public class PauseMenu : MonoBehaviour
     public static bool gameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject fpsCam;
+
+    void Start() {
+        fpsCam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
 
     void Update() {
         if(Input.GetKeyDown(KeyCode.Escape)) {
@@ -21,19 +26,22 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume() {
         pauseMenuUI.SetActive(false);
+        fpsCam.GetComponent<CameraController>().LockCursor();
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
 
     void Pause() {
         pauseMenuUI.SetActive(true);
+        fpsCam.GetComponent<CameraController>().UnlockCursor();
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
 
     public void LoadMenu() {
-        Time.timeScale = 1f;
         Debug.Log("Loading menu...");
+        
+        //Time.timeScale = 1f;
         //SceneManager.LoadScene("Menu");
     }
 
