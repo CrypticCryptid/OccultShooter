1f180f9f9c62c3f44660f90c91cadae47f7d230c
AnonymousAI <aege1.1.2002@gmail.com>
2024-05-31 14:24:04 -0700

    Fixing Inventory Code

--- Occult Shooter/Assets/Scripts/Inventory Scripts/Interactable.cs
+++ Occult Shooter/Assets/Scripts/Inventory Scripts/Interactable.cs
@@ -5,7 +5,6 @@ public class Interactable : MonoBehaviour
     public float radius = 3f;
     public Transform interactionTransform;
 
-    bool isFocus = false;
     Transform player;
 
     bool hasInteracted = false;
@@ -15,26 +14,20 @@ public class Interactable : MonoBehaviour
         Debug.Log("Interacting with " + transform.name);
     }    
 
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     void Update() {
-        if(isFocus && !hasInteracted) {
-            float distance = Vector3.Distance(player.position, interactionTransform.position);
-            if(distance <= radius) {
-                Interact();
-                hasInteracted = true;
+        if(Input.GetButtonDown("Interact")) {
+            if(!hasInteracted) {
+                float distance = Vector3.Distance(player.position, interactionTransform.position);
+                if(distance <= radius) {
+                    Interact();
+                    hasInteracted = true;
+                }
             }
         }
-    }
-
-    public void OnFocused(Transform playerTransform) {
-        isFocus = true;
-        player = playerTransform;
-        hasInteracted = false;
-    }
-
-    public void OnDefocused() {
-        isFocus = false;
-        player = null;
-        hasInteracted = false;
     }
 
     void OnDrawGizmosSelected() {
